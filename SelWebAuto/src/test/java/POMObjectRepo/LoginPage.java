07c1a0b0b7949f07de9e4a85e0a5e07b4d43cdc5
Rahul <kakaderahul70@gmail.com>
2024-01-29 23:42:42 +0530

    Second commit

--- SelWebAuto/src/test/java/POMObjectRepo/LoginPage.java
+++ SelWebAuto/src/test/java/POMObjectRepo/LoginPage.java
@@ -1,12 +1,50 @@
 package POMObjectRepo;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+//import org.testng.annotations.Test;
 
-import Base.Base;
+
 
 public class LoginPage {
 
+	WebDriver localDriver;
+	public LoginPage(WebDriver driver) {
+		
+		localDriver=driver;
+		PageFactory.initElements(driver,this);	
+	}
+	
+	@FindBy(xpath="//input[@id='userEmail']") private WebElement email;
+	
+	@FindBy(xpath="//input[@id='userPassword']") private WebElement password;
+	
+	@FindBy(xpath="//input[@id='login']") private WebElement loginBtn;
+	
+	public void enterEmail(String emailid) {
+		
+		email.sendKeys(emailid);
+		
+	}
+	
+	public void enterPassword(String pwd) {
+		
+		password.sendKeys(pwd);
+		
+	}
+	
+	public void clickLoginBtn() {
+		
+		loginBtn.click();
+		
+	}
 	
 	
 	
 }
+
+   
+   
+   
--- SelWebAuto/src/test/java/TestCases/LoginTest.java
+++ SelWebAuto/src/test/java/TestCases/LoginTest.java
@@ -1,22 +1,65 @@
 package TestCases;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import Base.Base;
+import POMObjectRepo.LoginPage;
 
 public class LoginTest extends Base{
+	LoginPage loginobj;
 	
+//	@Test
+//	public void loginTestMethod() {
+//		
+//		// step 1:Open browser 
+//		
+//		// step 2: enter url
+//		String Baseurl=read.getBrowserUrl();
+//		System.out.println("Baseurl: "+Baseurl);
+//		driver.get("https://rahulshettyacademy.com/client");
+//		
+//		// step 3: enter email, pwd and click on login button.
+//		loginobj=new LoginPage(driver);
+//	
+//		
+//		loginobj.enterEmail("kakaderahul70@gmail.com");
+//		loginobj.enterPassword("Rahul@123");
+//		loginobj.clickLoginBtn();
+//		
+//		
+//	}
+//	
 	@Test
-	public void loginTestMethod() {
+	public void loginTestWithInvalidData() throws InterruptedException {
 		
 		// step 1:Open browser 
 		
-		// step 2: enter url
-		String Baseurl=read.getBrowserUrl();
-		System.out.println("Baseurl: "+Baseurl);
-		driver.get("https://rahulshettyacademy.com/client");
-		
-		
+				// step 2: enter url
+				String Baseurl=read.getBrowserUrl();
+				System.out.println("Baseurl: "+Baseurl);
+				driver.get("https://rahulshettyacademy.com/client");
+				driver.manage().window().maximize();
+				
+				// step 3: enter email, pwd and click on login button.
+				loginobj=new LoginPage(driver);
+			
+				
+				loginobj.enterEmail("kakaderahul70@gmail.com");
+				loginobj.enterPassword("Rahul@1234");
+				loginobj.clickLoginBtn();
+				Thread.sleep(20000);
+				//Alert alert=driver.switchTo().alert();
+				//String errorMessage=alert.getText();
+				 WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+			     WebElement errorMessageElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("error-message")));
+				System.out.println("errorMessage: "+errorMessageElement);
 		
 	}
 	
